98da8842f2a96dbc5902ef34add8e2223c300e07
Rahul Sharma <rahulsharmados@gmail.com>
2025-07-24 22:52:52 +0530

    Update tables

--- LibraryManagementSystem.sql
+++ LibraryManagementSystem.sql
@@ -1,48 +1,63 @@
-CREATE TABLE libraryName(
+CREATE DATABASE libraryDB;
+USE libraryDB;
+
+
+CREATE TABLE Libraries(
     library_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    campus_location INT,
-    contact_email VARCHAR(50),
-    phone_number VARCHAR(30)
+    library_name VARCHAR(50) NOT NULL,
+    campus_location VARCHAR(50),
+    contact_email VARCHAR(50) UNIQUE,
+    phone_number VARCHAR(15) UNIQUE,
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
-CREATE TABLE Book   (
+CREATE TABLE Books   (
     book_id INT PRIMARY KEY,
-    title VARCHAR(50),
-    isbn VARCHAR(50),
+    title VARCHAR(50) NOT NULL,
+    isbn VARCHAR(20) UNIQUE,
     publication_date DATE,
-    total_copies INT,
-    available_copies INT,
+    total_copies INT DEFAULT 1,
+    available_copies INT DEFAULT 1,
     library_id INT,
-    FOREIGN KEY (library_id) REFERENCES libraryName(library_id)
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (library_id) REFERENCES Libraries(library_id)
 );
 
-CREATE TABLE Author    (
+CREATE TABLE Authors   (
     author_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     birth_date DATE,
     nationality VARCHAR(50),
-    biography VARCHAR(50)
+    biography TEXT,
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
-CREATE TABLE Category   (
+CREATE TABLE Categories   (
     category_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    description VARCHAR(50)
+    category  VARCHAR(50),
+    descriptions TEXT,
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Member     (
+CREATE TABLE Members    (
     member_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    email VARCHAR(50),
-    phone VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    phone_number VARCHAR(15) UNIQUE,
     member_type VARCHAR(20),
     registration_date DATE,
-    CHECK (member_type IN ('student', 'faculty'))
+    CHECK (member_type IN ('student', 'faculty')),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    
 );
 
 
@@ -50,33 +65,36 @@ CREATE TABLE Borrowing    (
     borrowing_id INT PRIMARY KEY,
     member_id INT,
     book_id INT,
-    borrow_date DATE,
-    due_date DATE,
+    borrow_date DATE NOT NULL,
+    due_date DATE NOT NULL,
     return_date DATE,
-    late_fee INT,
-    FOREIGN KEY (member_id) REFERENCES Member(member_id),
-    FOREIGN KEY (book_id) REFERENCES Book(book_id)  
+    late_fee DECIMAL(6,2),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)  
 );
 
 
-CREATE TABLE Review (
+CREATE TABLE Reviews (
     review_id INT PRIMARY KEY,
     member_id INT,
     book_id INT,
-    rating INT,
-    comment TEXT,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
     review_date DATE,
-    CHECK (rating BETWEEN 1 AND 5),
-    FOREIGN KEY (member_id) REFERENCES Member(member_id),
-    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     UNIQUE (member_id, book_id) 
 );
 
 CREATE TABLE BookAuthor (
     book_id INT,
     author_id INT,
-    FOREIGN KEY (author_id) REFERENCES Author(author_id),
-    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     PRIMARY KEY (book_id, author_id)  
 );
 
@@ -84,11 +102,10 @@ CREATE TABLE BookAuthor (
 CREATE TABLE BookCategory (
     book_id INT,
     category_id INT,
-    FOREIGN KEY (category_id) REFERENCES Category(category_id),
-    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     PRIMARY KEY (book_id, category_id)
 );
-
 
 
 
